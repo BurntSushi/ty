cf61128e25fe7b5c678c8dd1fcfb85df7b52e2bc
Andrew Gallant <jamslam@gmail.com>
2013-03-29 01:04:51 -0400

    We don't need so many type variables.

--- tyvars.go
+++ tyvars.go
@@ -10,7 +10,7 @@ import (
 //	type myOwnTypeVariable TypeVariable
 //
 // to create your own type variable. For your convenience, this package
-// defines type variables for each letter in the range A-Z.
+// defines some type variables for you.
 type TypeVariable struct {
 	noImitation struct{}
 }
@@ -26,22 +26,3 @@ type D TypeVariable
 type E TypeVariable
 type F TypeVariable
 type G TypeVariable
-type H TypeVariable
-type I TypeVariable
-type J TypeVariable
-type K TypeVariable
-type L TypeVariable
-type M TypeVariable
-type N TypeVariable
-type O TypeVariable
-type P TypeVariable
-type Q TypeVariable
-type R TypeVariable
-type S TypeVariable
-type T TypeVariable
-type U TypeVariable
-type V TypeVariable
-type W TypeVariable
-type X TypeVariable
-type Y TypeVariable
-type Z TypeVariable
