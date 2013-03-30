6b544b177c7d34374b1d00355939432fac1d8fe3
Andrew Gallant <jamslam@gmail.com>
2013-03-30 01:24:17 -0400

    merge benches and tests

--- fun/list_test.go
+++ fun/list_test.go
@@ -24,3 +24,32 @@ func TestPointers(t *testing.T) {
 		{1}, {4}, {9}, {16}, {25},
 	})
 }
+
+func BenchmarkMapReflect(b *testing.B) {
+	b.StopTimer()
+	list := randIntSlice(1000)
+	square := func(a int) int {
+		return a * a
+	}
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		_ = Map(square, list).([]int)
+	}
+}
+
+func BenchmarkMapBuiltin(b *testing.B) {
+	b.StopTimer()
+	list := randIntSlice(1000)
+	square := func(a int) int {
+		return a * a
+	}
+	b.StartTimer()
+
+	for i := 0; i < b.N; i++ {
+		ret := make([]int, len(list))
+		for i := 0; i < len(list); i++ {
+			ret[i] = square(list[i])
+		}
+	}
+}
--- fun/sort_test.go
+++ fun/sort_test.go
@@ -20,3 +20,84 @@ func TestQuickSort(t *testing.T) {
 
 	assertDeep(t, sorted, []int{15, 10, 6, 5, 3, 1})
 }
+
+func BenchmarkSortReflect(b *testing.B) {
+	less := func(a, b int) bool { return a < b }
+
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		list := randIntSlice(1000)
+		b.StartTimer()
+
+		Sort(less, list)
+	}
+}
+
+func BenchmarkSortBuiltin(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		list := randIntSlice(1000)
+		b.StartTimer()
+
+		sort.Sort(sort.IntSlice(list))
+	}
+}
+
+func BenchmarkQuickSortReflect(b *testing.B) {
+	less := func(a, b int) bool { return a < b }
+
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		list := randIntSlice(1000)
+		b.StartTimer()
+
+		_ = QuickSort(less, list)
+	}
+}
+
+func BenchmarkQuickSortBuiltin(b *testing.B) {
+	less := func(a, b int) bool { return a < b }
+
+	quicksort := func(xs []int) []int {
+		ys := make([]int, len(xs))
+		copy(ys, xs)
+
+		var qsort func(left, right int)
+		var partition func(left, right, pivot int) int
+
+		qsort = func(left, right int) {
+			if left >= right {
+				return
+			}
+			pivot := (left + right) / 2
+			pivot = partition(left, right, pivot)
+
+			qsort(left, pivot-1)
+			qsort(pivot+1, right)
+		}
+		partition = func(left, right, pivot int) int {
+			vpivot := ys[pivot]
+			ys[pivot], ys[right] = ys[right], ys[pivot]
+
+			ind := left
+			for i := left; i < right; i++ {
+				if less(ys[i], vpivot) {
+					ys[i], ys[ind] = ys[ind], ys[i]
+					ind++
+				}
+			}
+			ys[ind], ys[right] = ys[right], ys[ind]
+			return ind
+		}
+
+		return ys
+	}
+
+	for i := 0; i < b.N; i++ {
+		b.StopTimer()
+		list := randIntSlice(1000)
+		b.StartTimer()
+
+		_ = quicksort(list)
+	}
+}
