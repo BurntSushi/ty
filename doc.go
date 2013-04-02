9a53150675cb5b6afc8fc2acda1f2dab62a0cbc6
Andrew Gallant <jamslam@gmail.com>
2013-04-02 01:31:17 -0400

    package doco for ty

---
+++ doc.go
@@ -0,0 +1,15 @@
+/*
+Package ty provides utilities for writing type parametric functions with run
+time safety.
+
+Go tip (1.1) is required
+
+The very foundation of this package only recently became possible with the
+addition of 3 new functions in the standard library `reflect` package:
+SliceOf, MapOf and ChanOf. In particular, it provides the ability to
+dynamically construct types at run time from component types.
+
+Further extensions to this package can be made if similar functions are added
+for structs and functions(?).
+*/
+package ty
